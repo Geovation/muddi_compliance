79c03183fcb9502e26deb770fc09a472c208d6af
aspinrebecca <130049578+aspinrebecca@users.noreply.github.com>
2023-10-26 11:57:40 +0100

    update to electricity views including network

--- tools/views/views.sql
+++ tools/views/views.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA layers;
 
-CREATE VIEW layers.electricity_lines AS
+CREATE VIEW layers.electricity_links AS
 SELECT
     object.id,
     network_link.conveyance_type,
@@ -34,3 +34,23 @@ INNER JOIN muddi.network_node ON network_conveyance.id = network_node.network_co
 INNER JOIN muddi.network on network_conveyance.network_id = network.id
 INNER JOIN muddi.organisations on asset.asset_owner_id = organisations.id
 WHERE network_asset.utility_type = 'electricity';
+
+CREATE VIEW layers.electricity_networks AS
+SELECT
+    object.id,
+    network.network_name,
+    network.commodity_type,
+    CASE 
+        WHEN network.parent_network_id IS NOT NULL THEN True
+        ELSE False
+    END AS subnetwork,
+    parent_network.network_name as parent_network_name,
+    organisations.name as asset_owner,
+    object.sf_geometry
+
+FROM muddi.object
+INNER JOIN muddi.asset on object.id = asset.object_id
+INNER JOIN muddi.network on asset.id = network.asset_id
+LEFT JOIN muddi.network as parent_network on network.parent_network_id = parent_network.id
+INNER JOIN muddi.organisations on asset.asset_owner_id = organisations.id
+WHERE network.commodity_type = 'electricity';
